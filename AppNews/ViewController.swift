01c67032375f39945e571df02934388f6fb06e63
Maxim <lisitsa.max@yandex.ru>
2019-09-26 15:17:44 +0300

    Initial Commit

---
+++ AppNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppNews
+//
+//  Created by Максим Лисица on 26/09/2019.
+//  Copyright © 2019 Максим Лисица. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
