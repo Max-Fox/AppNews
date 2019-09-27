a26f2b60b4830d474e7ceaa5af1e31d28c98b06b
Maxim <lisitsa.max@yandex.ru>
2019-09-27 16:41:45 +0300

    fix bug

---
+++ AppNews/ViewControllers/SettingViewController/SettingTableViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SettingTableViewController.swift
+//  AppNews
+//
+//  Created by Максим Лисица on 27/09/2019.
+//  Copyright © 2019 Максим Лисица. All rights reserved.
+//
+
+import UIKit
+
+class SettingTableViewController: UITableViewController {
+    @IBOutlet weak var switchIsDetail: UISwitch!
+    
+    var delegate: SettingsProtocol?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Настройки"
+        
+        switchIsDetail.isOn = UserDefaults.standard.value(forKey: "withDetail") as? Bool ?? false
+    }
+
+    @IBAction func pushSwitchAction(_ sender: UISwitch!) {
+        delegate?.didPressSwitchIsDetailNews(isOn: sender.isOn)
+    }
+    
+}
